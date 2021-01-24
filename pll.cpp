b128da4f93e3a168b90b2842418c132aca7dd876
Jason Duong <76750523+mag-jison@users.noreply.github.com>
2021-01-23 20:12:20 -0800

    Update pll.cpp

--- pll.cpp
+++ pll.cpp
@@ -9,17 +9,10 @@
 
 using namespace std;
 
-void hide_cursor(){
-    CONSOLE_CURSOR_INFO info;
-    info.dwSize = 100;
-    info.bVisible = FALSE;
-    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
-}
-
 int main() {
     Timer watch;
 	vector<string> pll = {"A(a)", "A(b)", "T", "F", "V", "U(b)", "Z", "H", "J(a)", "J(b)", "N(a)", "N(b)", "E", "G(a)", "G(b)", "G(c)", "G(d)", "Y", "R(a)", "R(b)"};
-    hide_cursor();
+    watch.hide_cursor();
 
 	cout << "Start";
 	while (_getch() == ' '){
